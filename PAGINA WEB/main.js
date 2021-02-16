38560e18c5f1d96798256004ccdc72a3ef8918b1
SANTIAGORF24 <57204688+SANTIAGORF24@users.noreply.github.com>
2021-02-15 19:56:40 -0500

    Delete main.js

--- PAGINA WEB/main.js
+++
@@ -1,9 +0,0 @@
-document.querySelector('.menu-btn').addEventListener('click', () => {
-    document.querySelector('.nav-menu').classList.toggle('show');
-})
-
-ScrollReveal().reveal('.showcase');
-ScrollReveal().reveal('.news-cards', { delay: 50});
-ScrollReveal().reveal('.cards-baner-two', { delay: 50});
-ScrollReveal().reveal('.cards-baner-one', { delay: 50});
-ScrollReveal().reveal('.social', { delay: 50});
